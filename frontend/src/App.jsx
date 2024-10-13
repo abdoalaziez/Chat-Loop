9fede93942ef9fc1e9cc92ea96959b8730720f44
Azaan Suhail <azaan.suhail@gmail.com>
2024-10-13 14:24:25 +0530

    Connected signup to backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,22 @@
-import './App.css'
-import Home from './pages/home/Home'
-import Login from './pages/login/Login'
-import SignUp from './pages/signup/SignUp'
+import { Toaster } from "react-hot-toast";
+import "./App.css";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
+import { Routes, Route } from "react-router-dom";
 
-function App () {
+
+function App() {
   return (
-    <div className='p-4 h-screen flex items-center justify-center'>
-     <Home/>
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+      <Toaster/>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
