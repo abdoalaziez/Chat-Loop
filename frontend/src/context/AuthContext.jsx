71ea461899a7a7e702e0e644e7f819328e449f5e
Azaan Suhail <azaan.suhail@gmail.com>
2024-10-13 20:26:53 +0530

    More functionality 3.27

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,14 +1,12 @@
 import { createContext, useContext, useState } from "react";
 
-// Create the AuthContext
 export const AuthContext = createContext();
 
-// Custom hook to use the AuthContext
+// eslint-disable-next-line react-refresh/only-export-components
 export const useAuthContext = () => {
   return useContext(AuthContext);
 };
 
-// AuthContextProvider component
 export const AuthContextProvider = ({ children }) => {
   const [authUser, setAuthUser] = useState(
     JSON.parse(localStorage.getItem("chat-user")) || null
