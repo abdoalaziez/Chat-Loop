aff2064677139976f1ab54038939d02ede08e17a
Azaan Suhail <azaan.suhail@gmail.com>
2024-10-13 17:44:45 +0530

    Zustand global state management 3.14

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -92,7 +92,7 @@ const SignUp = () => {
             onCheckboxChange={handleCheckboxChange}
             selectedGender={inputs.gender}
           />
-        
+
           <Link
             to={"/login"}
             className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block"
@@ -102,8 +102,15 @@ const SignUp = () => {
           </Link>
 
           <div>
-            <button className="btn btn-block btn-sm mt-2 border border-slate-700">
-              SignUp
+            <button
+              className="btn btn-block btn-sm mt-2 border border-slate-700"
+              disabled={loading}
+            >
+              {loading ? (
+                <span className="loading loading-spinner"></span>
+              ) : (
+                "Sign Up"
+              )}
             </button>
           </div>
         </form>
