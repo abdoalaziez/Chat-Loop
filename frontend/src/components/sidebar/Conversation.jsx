5a3f7decd02bba58d4d02573b654d4ca9ae63965
Azaan Suhail <azaan.suhail@gmail.com>
2024-10-13 23:30:35 +0530

    almost completed

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import useConversation from '../../zustand/useConversation';
+import React from "react";
+import useConversation from "../../zustand/useConversation";
+import { useSocketContext } from "../../context/SocketContext";
 
-const Conversation = ({ conversation,  emoji,lastIdx }) => {
+const Conversation = ({ conversation, emoji, lastIdx }) => {
+  const { selectedConversation, setSelectedConversation } = useConversation();
+  const isSelected = selectedConversation?._id === conversation._id;
 
- const { selectedConversation, setSelectedConversation } = useConversation();
-	const isSelected = selectedConversation?._id === conversation._id;
-
-
+  const { onlineUsers } = useSocketContext()
+  const isOnline = onlineUsers.includes(conversation._id);
 
   return (
     <>
@@ -16,7 +17,7 @@ const Conversation = ({ conversation,  emoji,lastIdx }) => {
 			`}
         onClick={() => setSelectedConversation(conversation)}
       >
-        <div className="">
+        <div className={`avatar ${isOnline ? "online" : ""}`}>
           <div className="w-12 rounded-full">
             <img src={conversation.profilePic} alt="user avatar" />
           </div>
@@ -35,4 +36,4 @@ const Conversation = ({ conversation,  emoji,lastIdx }) => {
   );
 };
 
-export default Conversation
+export default Conversation;
