8154643b678d719eb70e71705450d35420d2d3c9
Azaan Suhail <azaan.suhail@gmail.com>
2024-10-13 22:19:52 +0530

    search functionality completed

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,16 +1,21 @@
-import React, { useRef } from 'react'
-import Message from './Message'
-import useGetMessages from '../../../hooks/useGetMessages'
-import MessageSkeleton from '../skeletons/MessageSkeleton'
+import React, { useRef,useEffect } from "react";
+import Message from "./Message";
+import useGetMessages from "../../../hooks/useGetMessages";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
 
 const Messages = () => {
-  const {messages,loading}=useGetMessages();
-  console.log("messages",messages);
-   const lastMessageRef = useRef()
+  const { messages, loading } = useGetMessages();
+
+  const lastMessageRef = useRef(); //by default it is null
+
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, 100);
+  }, [messages]);
 
   return (
     <div className="px-4 flex-1 overflow-auto">
-
       {!loading &&
         messages.length > 0 &&
         messages.map((message) => (
@@ -19,15 +24,13 @@ const Messages = () => {
           </div>
         ))}
 
-
       {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
 
       {!loading && messages.length === 0 && (
         <p className="text-center">Send a message to start the conversation</p>
       )}
-      
     </div>
   );
-}
+};
 
-export default Messages
+export default Messages;
